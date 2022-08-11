5bd7840262742d8b73d948497e6fb501f0797360
marogoba <marogoba@gmail.com>
2022-08-10 21:24:40 -0300

    Mesa de trabalho 10

---
+++ MesaTrabalho_A10/Main.java
@@ -0,0 +1,45 @@
+package MesaTrabalho_A10;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    private static final Logger logger = Logger.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        BasicConfigurator.configure();
+
+        List<Integer> listaInteiros = new ArrayList<>();
+
+        listaInteiros.add(10);
+        listaInteiros.add(10);
+        listaInteiros.add(10);
+        listaInteiros.add(10);
+        listaInteiros.add(10);
+        listaInteiros.add(10);
+
+        Inteiros i = new Inteiros(listaInteiros);
+
+        try {
+            i.calculodeItens();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        List<Integer> listaInteiros1 = new ArrayList<>();
+
+
+        Inteiros i1 = new Inteiros(listaInteiros1);
+
+        try {
+            i1.calculodeItens();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
